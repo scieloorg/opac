c28d262e3f4d9bc92b3f32310d75f83ae9ce9843
Herz <achwazer@gmail.com>
2016-03-15 14:39:15 -0600

    Agregados tests para admin.custom_filters

--- app/admin/custom_filters.py
+++ app/admin/custom_filters.py
@@ -12,9 +12,9 @@ from opac_schema.v1.models import Journal, Issue
 def get_flt(column=None, value=None, term=''):
     flt = None
     search_fields = {
-        'journal': ['jid', 'title', 'title_iso', 'short_title', 'acronym', 'print_issn', 'eletronic_issn'],
+        'journal': ['title', 'title_iso', 'short_title', 'acronym', 'print_issn', 'eletronic_issn'],
         'issue': ['label'],
-        'use_licenses': ['license_code']
+        'use_licenses': ['license_code', 'reference_url', 'disclaimer']
     }
 
     if isinstance(column, ReferenceField):
@@ -32,7 +32,7 @@ def get_flt(column=None, value=None, term=''):
                 criteria |= q
         if isinstance(column.document_type_obj(), Journal):
             reference_values = Journal.objects.filter(criteria)
-        elif isinstance(column.document_type_obj(), Issue):
+        if isinstance(column.document_type_obj(), Issue):
             reference_values = Issue.objects.filter(criteria)
         flt = {'%s__in' % column.name: reference_values}
 
@@ -111,13 +111,19 @@ class CustomFilterConverter(FilterConverter):
     # deve ser do tipo StringField
 
     reference_filters = (CustomFilterLike, CustomFilterNotLike, CustomFilterEqual,
+                         CustomFilterNotEqual, CustomFilterInList, CustomFilterNotInList)
+    embedded_filters = (CustomFilterLike, CustomFilterNotLike, CustomFilterEqual,
                          CustomFilterNotEqual, CustomFilterEmpty, CustomFilterInList,
                          CustomFilterNotInList)
     list_filters = (CustomFilterLike, CustomFilterNotLike, CustomFilterEmpty)
 
-    @filters.convert('ReferenceField', 'EmbeddedDocumentField')
+    @filters.convert('ReferenceField')
     def conv_reference(self, column, name):
         return [f(column, name) for f in self.reference_filters]
+
+    @filters.convert('EmbeddedDocumentField')
+    def conv_embedded(self, column, name):
+        return [f(column, name) for f in self.embedded_filters]
 
     @filters.convert('ListField')
     def conv_list(self, column, name):
