09b41771541073e43a07824f8638f8c332f4ea55
Juan Funez <juan.funez@gmail.com>
2016-04-11 13:52:00 -0300

    ajustes para os testes passarem, e form_ajax_refs nos campos resource nas admin views

---
+++ opac/webapp/admin/ajax.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from flask_admin._compat import as_unicode
+from flask_admin.contrib.mongoengine.ajax import QueryAjaxModelLoader, DEFAULT_PAGE_SIZE
+
+
+class CustomQueryAjaxModelLoader(QueryAjaxModelLoader):
+
+    def __init__(self, name, model, **options):
+        """
+            Constructor.
+            :param fields:
+                Fields to run query against
+        """
+        super(CustomQueryAjaxModelLoader, self).__init__(name, model, **options)
+
+    def format(self, model):
+        # mudança minima porém necessária, o atributo id no modelo é _id
+        if not model:
+            return None
+        return (as_unicode(model._id), as_unicode(model))
+
+    def get_one(self, pk):
+        # mudança minima porém necessária, o atributo id no modelo é _id
+        return self.model.objects.filter(_id=pk).first()
