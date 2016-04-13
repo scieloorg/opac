3cd78f9d1337b461f0e6f12c272ea46c10815bc5
Juan Funez <juan.funez@gmail.com>
2016-04-13 17:28:43 -0300

    #149 - Conteúdo dinâmico no menu Superior.
    FIX: #149

--- opac/tests/test_interface_header.py
+++ opac/tests/test_interface_header.py
@@ -201,3 +201,84 @@ class MenuTestCase(BaseTestCase):
         self.assertTemplateUsed('collection/about.html')
         expected_anchor = u'<a href="/about"\n         class="btn single dropdown-toggle selected">\n        <span class="glyphBtn infoMenu"></span>\n        <span class="hidden-sm">Sobre o SciELO</span>'
         self.assertIn(expected_anchor, response.data.decode('utf-8'))
+
+    # menu hamburger
+
+    def test_links_in_hamburger_menu(self):
+        """
+        no menú de hamurger, verificamos os links que apontam a views do opac
+        """
+        response = self.client.get(url_for('main.index'))
+        self.assertStatus(response, 200)
+        expected_anchor1 = u"""<a href="%s"><strong>SciELO Brasil</strong></a>""" % url_for('.index')
+        self.assertIn(expected_anchor1, response.data.decode('utf-8'))
+        expected_anchor2 = u"""<li><a href="%s">Lista alfabética de periódicos</a></li>""" % url_for('.collection_list_alpha')
+        self.assertIn(expected_anchor2, response.data.decode('utf-8'))
+        expected_anchor3 = u"""<li><a href="%s">Lista temática de periódicos</a></li>""" % url_for('.collection_list_theme')
+        self.assertIn(expected_anchor3, response.data.decode('utf-8'))
+        expected_anchor4 = u"""<li><a href="%s">Busca</a></li>""" % url_for('.search')
+        self.assertIn(expected_anchor4, response.data.decode('utf-8'))
+        expected_anchor5 = u"""<li><a href="%s">Métricas</a></li>""" % url_for('.metrics')
+        self.assertIn(expected_anchor5, response.data.decode('utf-8'))
+        expected_anchor6 = u"""<li><a href="%s">Sobre o SciELO Brasil</a></li>""" % url_for('.about')
+        self.assertIn(expected_anchor6, response.data.decode('utf-8'))
+        expected_anchor7 = u"""<li><a href="#">Contatos</a></li>"""
+        self.assertIn(expected_anchor7, response.data.decode('utf-8'))
+        expected_anchor8 = u"""<a href="#"><strong>SciELO.org - Rede SciELO</strong></a>"""
+        # rede/scielo org
+        self.assertIn(expected_anchor8, response.data.decode('utf-8'))
+        expected_anchor9 = u"""<li><a href="http://www.scielo.org/php/index.php">Coleções nacionais e temáticas</a></li>"""
+        self.assertIn(expected_anchor9, response.data.decode('utf-8'))
+        expected_anchor10 = u"""<li><a href="http://www.scielo.org/applications/scielo-org/php/secondLevel.php?xml=secondLevelForAlphabeticList&xsl=secondLevelForAlphabeticList">Lista alfabética de periódicos</a></li>"""
+        self.assertIn(expected_anchor10, response.data.decode('utf-8'))
+        expected_anchor11 = u"""<li><a href="http://www.scielo.org/applications/scielo-org/php/secondLevel.php?xml=secondLevelForSubjectByLetter&xsl=secondLevelForSubjectByLetter">Lista de periódicos por assunto</a></li>"""
+        self.assertIn(expected_anchor11, response.data.decode('utf-8'))
+        expected_anchor12 = u"""<li><a href="http://search.scielo.org/">Busca</a></li>"""
+        self.assertIn(expected_anchor12, response.data.decode('utf-8'))
+        expected_anchor13 = u"""<li><a href="http://www.scielo.org/applications/scielo-org/php/siteUsage.php">Métricas</a></li>"""
+        self.assertIn(expected_anchor13, response.data.decode('utf-8'))
+        expected_anchor14 = u"""<li><a href="http://www.scielo.org/php/level.php?lang=pt&component=56&item=9">Acesso OAI e RSS</a></li>"""
+        self.assertIn(expected_anchor14, response.data.decode('utf-8'))
+        expected_anchor15 = u"""<li><a href="http://www.scielo.org/php/level.php?lang=pt&component=56&item=8">Sobre a Rede SciELO</a></li>"""
+        self.assertIn(expected_anchor15, response.data.decode('utf-8'))
+        expected_anchor16 = u"""<li><a href="#">Contatos</a></li>"""
+        self.assertIn(expected_anchor16, response.data.decode('utf-8'))
+        expected_anchor17 = u"""<a href="#"><strong>Portal do Autor</strong></a>"""
+        self.assertIn(expected_anchor17, response.data.decode('utf-8'))
+
+    def test_blog_link_in_hamburger_manu(self):
+        """
+        Verificamos que o link para o blog em perspectiva fique
+        apontando ao link certo considerando o idioma da sessão
+        """
+        with self.client as c:
+            collection = utils.makeOneCollection()
+            # idioma em 'pt_br'
+            response = c.get(
+                url_for('main.set_locale', lang_code='pt_BR'),
+                headers={'Referer': '/'},
+                follow_redirects=True)
+
+            self.assertStatus(response, 200)
+            expected_anchor = '<a href="http://blog.scielo.org/">'
+            self.assertIn(expected_anchor, response.data.decode('utf-8'))
+
+            # idioma em 'en'
+            response = c.get(
+                url_for('main.set_locale', lang_code='en'),
+                headers={'Referer': '/'},
+                follow_redirects=True)
+
+            self.assertStatus(response, 200)
+            expected_anchor = '<a href="http://blog.scielo.org/en/">'
+            self.assertIn(expected_anchor, response.data.decode('utf-8'))
+
+            # idioma em 'es'
+            response = c.get(
+                url_for('main.set_locale', lang_code='es'),
+                headers={'Referer': '/'},
+                follow_redirects=True)
+
+            self.assertStatus(response, 200)
+            expected_anchor = '<a href="http://blog.scielo.org/es/">'
+            self.assertIn(expected_anchor, response.data.decode('utf-8'))
