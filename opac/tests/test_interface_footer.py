bb1c768e13c291bee690afcb14bab66a19242b22
Jamil Atta Junior <atta.jamil@gmail.com>
2016-04-13 18:06:00 -0300

    corrigindo os testes para passar no travis

--- opac/tests/test_interface_footer.py
+++ opac/tests/test_interface_footer.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from flask import url_for
+from flask import url_for, current_app
 from base import BaseTestCase
 
 import utils
@@ -14,39 +14,40 @@ class FooterTestCase(BaseTestCase):
         cadastrada na coleção.
         """
 
-        collection = utils.makeOneCollection({'license': 'BY/4.0'})
+        with current_app.app_context():
+            collection = utils.makeOneCollection({'license': 'BY/4.0'})
 
-        with self.client as c:
+            with self.client as c:
 
-            response = c.get(url_for('main.index'))
+                response = c.get(url_for('main.index'))
 
-            self.assertStatus(response, 200)
+                self.assertStatus(response, 200)
 
-            self.assertTemplateUsed('collection/index.html')
+                self.assertTemplateUsed('collection/index.html')
 
-            self.assertIn('https://creativecommons.org/licenses/by/4.0/', response.data)
-            self.assertIn('https://i.creativecommons.org/l/by/4.0/88x31.png', response.data)
-            self.assertIn('Todo o conteúdo de localhost, exceto onde está identificado, está licenciado sob uma Licença Creative Commons.', response.data)
+                self.assertIn('https://creativecommons.org/licenses/by/4.0/', response.data)
+                self.assertIn('https://i.creativecommons.org/l/by/4.0/88x31.png', response.data)
+                self.assertIn('Todo o conteúdo de localhost, exceto onde está identificado, está licenciado sob uma Licença Creative Commons.', response.data)
 
     def test_license_list_theme(self):
         """
         Testa se os links e o conteúdo da licença esta de acordo com a licença
         cadastrada na coleção.
         """
+        with current_app.app_context():
+            collection = utils.makeOneCollection({'license': 'BY/4.0'})
 
-        collection = utils.makeOneCollection({'license': 'BY/4.0'})
+            with self.client as c:
 
-        with self.client as c:
+                response = c.get(url_for('main.collection_list_theme'))
 
-            response = c.get(url_for('main.collection_list_theme'))
+                self.assertStatus(response, 200)
 
-            self.assertStatus(response, 200)
+                self.assertTemplateUsed('collection/list_theme.html')
 
-            self.assertTemplateUsed('collection/list_theme.html')
-
-            self.assertIn('https://creativecommons.org/licenses/by/4.0/', response.data)
-            self.assertIn('https://i.creativecommons.org/l/by/4.0/88x31.png', response.data)
-            self.assertIn('Todo o conteúdo de localhost, exceto onde está identificado, está licenciado sob uma Licença Creative Commons.', response.data)
+                self.assertIn('https://creativecommons.org/licenses/by/4.0/', response.data)
+                self.assertIn('https://i.creativecommons.org/l/by/4.0/88x31.png', response.data)
+                self.assertIn('Todo o conteúdo de localhost, exceto onde está identificado, está licenciado sob uma Licença Creative Commons.', response.data)
 
     def test_license_journal_home(self):
         """
@@ -54,19 +55,20 @@ class FooterTestCase(BaseTestCase):
         cadastrada na coleção.
         """
 
-        collection = utils.makeOneCollection({'license': 'BY/4.0'})
+        with current_app.app_context():
+            collection = utils.makeOneCollection({'license': 'BY/4.0'})
 
-        journal = utils.makeOneJournal()
+            journal = utils.makeOneJournal()
 
-        with self.client as c:
+            with self.client as c:
 
-            response = c.get(url_for('main.journal_detail',
-                                     journal_id=journal.id))
+                response = c.get(url_for('main.journal_detail',
+                                         journal_id=journal.id))
 
-            self.assertStatus(response, 200)
+                self.assertStatus(response, 200)
 
-            self.assertTemplateUsed('journal/detail.html')
+                self.assertTemplateUsed('journal/detail.html')
 
-            self.assertIn('https://creativecommons.org/licenses/by/4.0/', response.data)
-            self.assertIn('https://i.creativecommons.org/l/by/4.0/88x31.png', response.data)
-            self.assertIn('Todo o conteúdo de localhost, exceto onde está identificado, está licenciado sob uma Licença Creative Commons.', response.data)
+                self.assertIn('https://creativecommons.org/licenses/by/4.0/', response.data)
+                self.assertIn('https://i.creativecommons.org/l/by/4.0/88x31.png', response.data)
+                self.assertIn('Todo o conteúdo de localhost, exceto onde está identificado, está licenciado sob uma Licença Creative Commons.', response.data)
