837fd0f828fabafe8279de53c7b7114531e564e8
Jamil Atta Junior <atta.jamil@gmail.com>
2016-10-17 18:10:44 -0200

    Javascript Assets

---
+++ opac/webapp/static/js/admin/common.js
@@ -0,0 +1,16 @@
+$('.popup').popupWindow({
+    scrollbars:1,
+    resizable:1,
+    centerScreen:1,
+    width:600,
+    height:300
+});
+
+
+$('.bigpopup').popupWindow({
+    scrollbars:1,
+    resizable:1,
+    centerScreen:1,
+    width:1024,
+    height:700
+});
