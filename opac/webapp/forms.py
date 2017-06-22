1c7943c8a12428064cada2883a2fe26212e29f6c
Jamil Atta Junior <atta.jamil@gmail.com>
2017-06-22 17:20:23 -0300

    Corrigido o fluxo completo de envio de e-mail

---
+++ opac/webapp/forms.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+import re
+from flask_wtf import FlaskForm
+from flask_babelex import gettext as _
+from wtforms import StringField, TextAreaField, HiddenField
+from wtforms.validators import DataRequired, Email, URL, ValidationError
+
+
+class EmailShareForm(FlaskForm):
+    your_email = StringField('your_email', validators=[DataRequired(), Email()])
+    recipients = StringField('recipients', validators=[DataRequired()])
+    share_url = HiddenField('share_url', validators=[URL(), DataRequired()])
+    subject = StringField('subject')
+    comment = TextAreaField('comment')
+
+    def validate_recipients(form, field):
+        regex = re.compile(r'^.+@([^.@][^@]+)$', re.IGNORECASE)
+
+        splitted_emails = field.data.split(';')
+
+        for email in splitted_emails:
+            email = email.strip()
+
+            if email:
+                match = regex.match(email or '')
+
+                if not match:
+                    raise ValidationError(_('Invalid email address.'))
