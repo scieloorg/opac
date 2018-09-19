8a3e18b2b0b98900faf623591f12ccf72c0bfaa8
robertatakenaka <roberta.takenaka@scielo.org>
2018-09-19 09:31:32 -0300

    [tk983] Atualizar a versão de opac_schema para 2.47
    Fixes #983

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ install_requirements = [
 ]
 
 dependency_links = [
-    'http://github.com/scieloorg/opac_schema/tarball/v2.44#egg=opac_schema-v2.44'
+    'http://github.com/scieloorg/opac_schema/tarball/v2.47#egg=opac_schema-v2.47'
 ]
 
 setup(
