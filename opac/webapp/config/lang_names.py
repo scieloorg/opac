9b0af6900d05d100553c382daf93cf3719aa6fda
robertatakenaka <roberta.takenaka@scielo.org>
2018-12-11 09:53:47 -0200

    [tk833] Documentação do lang_names

--- opac/webapp/config/lang_names.py
+++ opac/webapp/config/lang_names.py
@@ -1,5 +1,7 @@
+# coding=utf-8
 
 # Source: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes, 10 Dec 2018
+# Pares são (nome original, nome ISO)
 
 LANG_NAMES = {
     "ab": (u"аҧсуа бызшәа, аҧсшәа", u"Abkhazian"),
