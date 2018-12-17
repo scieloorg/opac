494fc1da2bec7959d9292de68ca03e0f357b94e3
robertatakenaka <roberta.takenaka@scielo.org>
2018-12-17 09:48:47 -0200

    [tk891] Inclui teste para related_links

--- opac/tests/test_utils_related_articles.py
+++ opac/tests/test_utils_related_articles.py
@@ -39,3 +39,16 @@ class UtilsGScholarTestCase(BaseTestCase):
         self.assertEqual(len(result), 2)
         self.assertIn('cites=', result[0])
         self.assertIn('q=related:', result[1])
+
+    def test_related_links(self):
+        article_page_url = 'http://www.scielo.br/scielo.php?pid=S0100-879X2004000400003&script=sci_arttext'
+        titles = [
+            'Genome features of Leptospira interrogans serovar Copenhageni',
+        ]
+        result = related_links.related_links(article_page_url, titles)
+        self.assertEqual(result[0][0], "Google")
+        self.assertEqual(result[0][1], "Similares no")
+        self.assertEqual(result[0][2], "https://www.google.com/search?q=Genome features of Leptospira interrogans serovar Copenhageni")
+        self.assertEqual(result[1][0], "Google Scholar")
+        self.assertEqual(result[1][1], "Citados e Similares no")
+        self.assertEqual(result[1][2], 'https://scholar.google.com/scholar?q=Genome features of Leptospira interrogans serovar Copenhageni')
