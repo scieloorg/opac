8bb33f408df3dde0f1ebf9bdcaab605c8d907d8c
Eduardo Robles <robles.edu@gmail.com>
2019-05-17 15:18:50 -0300

    Remoção código para exibir autores na impressão

--- opac/webapp/static/js/scielo-article.js
+++ opac/webapp/static/js/scielo-article.js
@@ -370,22 +370,6 @@ var Article = {
 	    		$(this).find('.fm-button-main').removeClass('fm-button-main-mobile-open');
 	    	}
 	    });
-
-	    //executa o trigger de click para impressão quando houver mais de 10 autores em um artigo
-	    if($(".contribGroup [data-toggle='tooltip']").length > 0) {
-	    	window.addEventListener("beforeprint", function(event) {
-	    		$(".contribGroup [data-toggle='tooltip']").trigger("click");
-	    	});
-
-	    	//safari
-	    	var mediaArticlePrint = window.matchMedia('print');
-	    	mediaArticlePrint.addListener(function(map) {
-				if(map.matches) {
-					$(".contribGroup [data-toggle='tooltip']").trigger("click");
-				}
-			});
-	    }
-
 	},
 
 	isScrolledIntoView: function(elem){
