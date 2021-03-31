3dd59d59d6e82bc1a6f58f797c598c40ecd98717
Joffily Ferreira <joffily.ferreira@scielo.org>
2021-03-31 11:56:00 -0300

    Remove a contagem de artigos/fascículos/periódicos ao obter a coleção
    A função que obtém a coleção corrente da aplicação estava contabilizando
    a quantidade de documentos presentes nas coleções de artigos,
    fascículos e periódicos para todas as vezes que fosse chamada.
    
    Atualmente o único ponto da aplicação que utiliza essa contagem é
    a view da página inicial. Dessa forma é seguro isolar esse trecho
    de código apenas para o seu ponto de uso.

--- opac/webapp/controllers.py
+++ opac/webapp/controllers.py
@@ -77,22 +77,10 @@ class ArticleNotFoundError(Exception):
 def get_current_collection():
     """
     Retorna o objeto coleção filtrando pela coleção cadastrada no arquivo de
-    configuração ``OPAC_COLLECTION`` e atualiza com os dados da coleção já no site.
-    Isso se mantém ainda para coletar as citações que não são possíveis extrair dos
-    dados já presentes no site.
+    configuração ``OPAC_COLLECTION``.
     """
     current_collection_acronym = current_app.config['OPAC_COLLECTION']
     collection = Collection.objects.get(acronym=current_collection_acronym)
-    number_of_journals = Journal.objects.filter(
-        is_public=True, current_status="current").count()
-    if number_of_journals > 0:
-        collection.metrics.total_journal = number_of_journals
-    number_of_issues = Issue.objects.filter(is_public=True).count()
-    if number_of_issues > 0:
-        collection.metrics.total_issue = number_of_issues
-    number_of_articles = Article.objects.filter(is_public=True).count()
-    if number_of_articles > 0:
-        collection.metrics.total_article = number_of_articles
     return collection
 
 
