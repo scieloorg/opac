ae97940ce95ede061e7d0e3da55d2269734bfeb2
Jamil Atta Junior <atta.jamil@gmail.com>
2021-05-05 14:36:34 -0300

    Atualiza o arquivo gulp e suas dependências.

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,25 @@
-// include gulp  and path modules:
-var path = require('path');
-var gulp = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
+const path = require('path');
+const less = require('gulp-less');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const sourceMaps = require('gulp-sourcemaps');
+const connect = require('gulp-connect');
+const gutil = require('gulp-util');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-minify-css');
 
-// include plug-ins: gulp-concat, gulp-uglify, gulp-strip-debug, gulp-less
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-var stripDebug = require('gulp-strip-debug');
-var uglify = require('gulp-uglify');
-var less = require('gulp-less');
-var minifyCSS = require('gulp-minify-css');
-
-gulp.task('default', function() {
-  console.log('\nTasks para processar os JS:');
-  console.log('\t- process-scielo-bundle-js: para gerar o scielo-bundle.js');
-  console.log('\t- process-scielo-article-js: para gerar o scielo-article.js');
-  console.log('\t- process-scielo-article-standalone-js: para gerar o scielo-bundle.js');
-  console.log('\t- process-all-js: para processar todas as tasks (JS) anteriores num paso só.');
-  console.log('\nTasks para processar os LESS e gerar os CSS:');
-  console.log('\t- process-scielo-bundle-less: para gerar o scielo-bundle.less');
-  console.log('\t- process-scielo-article-less: para gerar o scielo-article.js');
-  console.log('\t- process-scielo-article-standalone-less: para gerar o scielo-article-standalone.less');
-  console.log('\t- process-scielo-bundle-print-less: para gerar o scielo-bundle-print.less');
-  console.log('\t- process-all-less: para processar todas as tasks (less) anteriores num paso só.');
-  console.log('\nTasks para processar TUDO:');
-  console.log('\t- process-all: chama as tasks: "process-all-js" e "process-all-less"');
-  console.log('\nTasks para monitorar (watch) arquivos LESS e JS:');
-  console.log('\t- watch-all: cada mudança de arquivos *.less e *.js (da pasta /static/) e chama: "process-all-less" e "process-all-js" respectivamente');
-});
 
 // caminho da pasta 'node_modules/bootstrap'
-var node_folder_path = path.join(__dirname, 'node_modules/');
+let node_folder_path = path.join(__dirname, 'node_modules/');
 console.info('[INFO] [gulpfile.js] path da pasta node_modules:\t', node_folder_path);
 
 // caminho da pasta 'opac/webapp/static/'
-var static_folder_path = path.join(__dirname, 'opac/webapp/static/');
+let static_folder_path = path.join(__dirname, 'opac/webapp/static/');
 console.info('[INFO] [gulpfile.js] path da pasta static:\t', static_folder_path);
 
-var paths = {
+let paths = {
 
     // caminho relativo da pasta 'bootstrap/js/ dentro do node_modules, fora do projeto'
     'bootstrap_js': path.join(node_folder_path, 'bootstrap/dist/js/'),
@@ -61,7 +44,7 @@ console.info('[INFO] [gulpfile.js] path da pasta static/js:\t', paths['static_js
 console.info('[INFO] [gulpfile.js] path da pasta static/less:\t', paths['static_less']);
 console.info('[INFO] [gulpfile.js] path da pasta static/css:\t', paths['static_css']);
 
-var target_src = {
+let target_src = {
     'js': {
         'scielo-bundle': [
             // instruções JS (designer)
@@ -110,7 +93,7 @@ var target_src = {
     }
 };
 
-var output = {
+let output = {
     'js': {
         'folder': paths['static_js'],
         // caminho dos arquivos JS para salvar os resultados
@@ -127,166 +110,74 @@ var output = {
     }
 };
 
-// Task para gerar o scielo-bundle.js
-gulp.task('process-scielo-bundle-js', function() {
-    var source_file = target_src['js']['scielo-bundle'];
-    var output_file = output['js']['scielo-bundle'];
-    var output_folder = output['js']['folder'];
-    console.info('[INFO] [task: process-scielo-bundle-js] - source file:\t', source_file);
-    console.info('[INFO] [task: process-scielo-bundle-js] - output file:\t', output_file);
-    console.info('[INFO] [task: process-scielo-bundle-js] - output folder:\t', output_folder);
-    gulp.src(source_file)
-        .pipe(concat(output_file))
-        .pipe(sourcemaps.init())
-        .pipe(stripDebug())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('../maps'))
-        .pipe(gulp.dest(output_folder)
+function processScieloBundleLess(){
+    return src(target_src['less']['scielo-bundle'])
+    .pipe(
+        sourceMaps.init()
+    )
+    .pipe(
+        less().on('error', function(err) {
+            gutil.log(err);
+            this.emit('end');
+        }))
+    .pipe(
+        cleanCSS()
+    )
+    .pipe(
+        minifyCSS()
+    )
+    .pipe(
+        dest(output['css']['folder'])
+    )
+    .pipe(
+        connect.reload()
     );
-});
+}
 
-// Task para gerar o scielo-article.js
-gulp.task('process-scielo-article-js', function() {
-    var source_file = target_src['js']['scielo-article'];
-    var output_file = output['js']['scielo-article'];
-    var output_folder = output['js']['folder'];
-    console.info('[INFO] [task: process-scielo-article-js] - source file:\n', source_file);
-    console.info('[INFO] [task: process-scielo-article-js] - output file:\t', output_file);
-    console.info('[INFO] [task: process-scielo-article-js] - output folder\t:', output_folder);
-    gulp.src(source_file)
-        .pipe(concat(output_file))
-        .pipe(sourcemaps.init())
-        .pipe(stripDebug())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('../maps'))
-        .pipe(gulp.dest(output_folder)
+function processScieloArticleLess(){
+    return src(target_src['less']['scielo-article'])
+    .pipe(
+        sourceMaps.init()
+    )
+    .pipe(
+        less().on('error', function(err) {
+            gutil.log(err);
+            this.emit('end');
+        }))
+    .pipe(
+        cleanCSS()
+    )
+    .pipe(
+        minifyCSS()
+    )
+    .pipe(
+        dest(output['css']['folder'])
+    )
+    .pipe(
+        connect.reload()
     );
-});
+}
 
-// Task para gerar o scielo-article-standalone.js
-gulp.task('process-scielo-article-standalone-js', function() {
-    var source_file = target_src['js']['scielo-article-standalone'];
-    var output_file = output['js']['scielo-article-standalone'];
-    var output_folder = output['js']['folder'];
-    console.info('[INFO] [task: process-scielo-article-standalone-js] - source file:\n', source_file);
-    console.info('[INFO] [task: process-scielo-article-standalone-js] - output file:\t', output_file);
-    console.info('[INFO] [task: process-scielo-article-standalone-js] - output folder:\t', output_folder);
-    gulp.src(source_file)
-        .pipe(concat(output_file))
-        .pipe(sourcemaps.init())
-        .pipe(stripDebug())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('../maps'))
-        .pipe(gulp.dest(output_folder)
-    );
-});
+function watchProcessScieloBundleLess() {
+    return watch(paths['static_less'], processScieloBundleLess);
+}
 
-// Task para rodar as tasks: "process-scielo-*-js" num passo só
-gulp.task('process-all-js',
-    [
-        'process-scielo-bundle-js',
-        'process-scielo-article-js',
-        'process-scielo-article-standalone-js',
-    ],
-    function() {}
+function watchProcessScieloArticleLess() {
+    return watch(paths['static_less'], processScieloArticleLess);
+}
+
+exports.watch = series(
+    processScieloBundleLess,
+    processScieloArticleLess,
+    
+    parallel(
+        watchProcessScieloBundleLess,
+        watchProcessScieloArticleLess
+    )
+)
+
+
+exports.default = series(
+    processScieloArticleLess
 );
 
-// Task para gerar o scielo-bundle.less
-gulp.task('process-scielo-bundle-less', function(){
-    var source_file = target_src['less']['scielo-bundle'];
-    var output_file = output['css']['scielo-bundle'];
-    var output_folder = output['css']['folder'];
-    console.info('[INFO] [task: process-scielo-bundle-less] - source file:\n', source_file);
-    console.info('[INFO] [task: process-scielo-bundle-less] - output file:\t', output_file);
-    console.info('[INFO] [task: process-scielo-bundle-less] - output folder:\t', output_folder);
-
-    gulp.src(source_file)
-        .pipe(concat(output_file))
-        .pipe(less(output_file))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest(output_folder)
-    );
-});
-
-// Task para gerar o scielo-article.less
-gulp.task('process-scielo-article-less', function(){
-    var source_file = target_src['less']['scielo-article'];
-    var output_file = output['css']['scielo-article'];
-    var output_folder = output['css']['folder'];
-    console.info('[INFO] [task: process-scielo-article-less] - source file:\n', source_file);
-    console.info('[INFO] [task: process-scielo-article-less] - output file:\t', output_file);
-    console.info('[INFO] [task: process-scielo-article-less] - output folder:\t', output_folder);
-
-    gulp.src(source_file)
-        .pipe(concat(output_file))
-        .pipe(less(output_file))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest(output_folder)
-    );
-});
-
-// Task para gerar o scielo-article-standalone.less
-gulp.task('process-scielo-article-standalone-less', function(){
-    var source_file = target_src['less']['scielo-article-standalone'];
-    var output_file = output['css']['scielo-article-standalone'];
-    var output_folder = output['css']['folder'];
-    console.info('[INFO] [task: process-scielo-article-standalone-less] - source file:\n', source_file);
-    console.info('[INFO] [task: process-scielo-article-standalone-less] - output file:\t', output_file);
-    console.info('[INFO] [task: process-scielo-article-standalone-less] - output folder:\t', output_folder);
-
-    gulp.src(source_file)
-        .pipe(concat(output_file))
-        .pipe(less(output_file))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest(output_folder)
-    );
-});
-
-// Task para gerar o scielo-bundle-print.less
-gulp.task('process-scielo-bundle-print-less', function(){
-    var source_file = target_src['less']['scielo-bundle-print'];
-    var output_file = output['css']['scielo-bundle-print'];
-    var output_folder = output['css']['folder'];
-    console.info('[INFO] [task: process-scielo-bundle-print-less] - source file:\n', source_file);
-    console.info('[INFO] [task: process-scielo-bundle-print-less] - output file:\t', output_file);
-    console.info('[INFO] [task: process-scielo-bundle-print-less] - output folder:\t', output_folder);
-
-    gulp.src(source_file)
-        .pipe(concat(output_file))
-        .pipe(less(output_file))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest(output_folder)
-    );
-});
-
-// Task para rodar as tasks: "process-scielo-*-less" num paso só
-gulp.task('process-all-less',
-    [
-        'process-scielo-bundle-less',
-        'process-scielo-article-less',
-        'process-scielo-article-standalone-less',
-        'process-scielo-bundle-print-less',
-    ],
-    function() {}
-);
-
-// Task para rodar as tasks: "process-scielo-*-js" e "process-scielo-*-less" num paso só
-gulp.task('process-all',
-    [
-        'process-all-js',
-        'process-all-less',
-    ],
-    function() {}
-);
-
-gulp.task('watch-all', function () {
-    // LESS
-    var less_files_glob = paths['static_less'] + '*.less';
-    console.info('[INFO] [task: watch-all] criando um watch para arquivos LESS:\t', less_files_glob);
-    var watcher_less = gulp.watch(less_files_glob, ['process-all-less']);
-
-    // JS
-    var js_files_glob = paths['static_js'] + '*.js';
-    console.info('[INFO] [task: watch-all] criando um watch para arquivos JS:\t', js_files_glob);
-    var watcher_js = gulp.watch(js_files_glob, ['process-all-js']);
-});
