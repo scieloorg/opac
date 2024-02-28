3bf8724d70587b1d947e73ab0256a39853f70bf3
Roberta Takenaka <505143+robertatakenaka@users.noreply.github.com>
2024-02-28 07:56:36 -0300

    Refatora a função fetch_data (#2925)
    * Corrige ausência de apresentação do rótulo dentro da seção  'Edited by'
    
    * Adiciona tenacity
    
    * Corrige parâmetro e comandos de atualização de pip e requirements.*
    
    * Melhora fetch_data para realizar retentativas

--- opac/webapp/main/views.py
+++ opac/webapp/main/views.py
@@ -31,6 +31,13 @@ from legendarium.formatter import descriptive_short_format
 from lxml import etree
 from opac_schema.v1.models import Article, Collection, Issue, Journal
 from packtools import HTMLGenerator
+from tenacity import (
+    retry,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_exponential,
+)
+
 from webapp import babel, cache, controllers, forms
 from webapp.choices import STUDY_AREAS
 from webapp.config.lang_names import display_original_lang_name
@@ -70,25 +77,53 @@ class NonRetryableError(Exception):
     """
 
 
-def fetch_data(url: str, timeout: float = 2) -> bytes:
-    try:
-        response = requests.get(url, timeout=timeout)
-    except (requests.ConnectionError, requests.Timeout) as exc:
-        raise RetryableError(exc) from exc
-    except (requests.InvalidSchema, requests.MissingSchema, requests.InvalidURL) as exc:
-        raise NonRetryableError(exc) from exc
-    else:
-        try:
-            response.raise_for_status()
-        except requests.HTTPError as exc:
-            if 400 <= exc.response.status_code < 500:
-                raise NonRetryableError(exc) from exc
-            elif 500 <= exc.response.status_code < 600:
-                raise RetryableError(exc) from exc
-            else:
-                raise
+@retry(
+    retry=retry_if_exception_type(RetryableError),
+    wait=wait_exponential(multiplier=1, min=1, max=5),
+    stop=stop_after_attempt(5),
+)
+def fetch_data(url, headers=None, json=False, timeout=4, verify=True):
+    """
+    Get the resource with HTTP
+    Retry: Wait 2^x * 1 second between each retry starting with 4 seconds,
+           then up to 10 seconds, then 10 seconds afterwards
+    Args:
+        url: URL address
+        headers: HTTP headers
+        json: True|False
+        verify: Verify the SSL.
+    Returns:
+        Return a requests.response object.
+    Except:
+        Raise a RetryableError to retry.
+    """
 
-    return response.content
+    try:
+        logger.info("Fetching the URL: %s" % url)
+        response = requests.get(url, headers=headers, timeout=timeout, verify=verify)
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as exc:
+        logger.error("Erro fetching the content: %s, retry..., erro: %s" % (url, exc))
+        raise RetryableError(exc) from exc
+    except (
+        requests.exceptions.InvalidSchema,
+        requests.exceptions.MissingSchema,
+        requests.exceptions.InvalidURL,
+    ) as exc:
+        raise NonRetryableError(exc) from exc
+    try:
+        response.raise_for_status()
+    except requests.HTTPError as exc:
+        if 400 <= exc.response.status_code < 500:
+            raise NonRetryableError(exc) from exc
+        elif 500 <= exc.response.status_code < 600:
+            logger.error(
+                "Erro fetching the content: %s, retry..., erro: %s" % (url, exc)
+            )
+            raise RetryableError(exc) from exc
+        else:
+            raise
+
+    return response.content if not json else response.json()
 
 
 @main.before_app_request
